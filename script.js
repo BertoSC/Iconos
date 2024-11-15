ec430946ca0026ace188e5f1a255faa6a22414ea
BertoSC <a23albertogc@iessanclemente.net>
2024-11-15 18:04:28 +0100

    ja

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function startDownload() {
     const arrow = document.querySelector('.download-arrow');
     const message = document.getElementById('downloadMessage');
 
-    arrow.classList.add('active'); // Oculta la flecha
+    arrow.classList.add('active'); 
 
-    // Crea partículas de "desfragmentación"
     for (let i = 0; i < 50; i++) {
         const piece = document.createElement('div');
         piece.classList.add('piece');
@@ -12,13 +11,12 @@ function startDownload() {
         piece.style.setProperty('--y', `${Math.random() * 200 - 100}px`);
         arrow.appendChild(piece);
 
-        // Quita las partículas después de la animación
+        
         setTimeout(() => {
             piece.remove();
         }, 500);
     }
 
-    // Muestra el mensaje después de la animación
     setTimeout(() => {
         arrow.style.display = 'none';
         message.style.opacity = '1';
--- subirvolumen/script.js
+++ subirvolumen/script.js
@@ -12,17 +12,16 @@ function decreaseVolume() {
     if (volume > 0) {
         volume--;
         updateVolume();
-        // Al reducir el volumen, no se muestra el chorro de agua
+    
     }
 }
 
 function updateVolume() {
-    // Actualizar el texto del nivel de volumen
     document.getElementById('volume-level').textContent = `Volumen: ${volume}`;
 
-    // Ajustar la altura del agua según el volumen
+    
     const water = document.querySelector('.water');
-    const waterHeight = (volume / 10) * 100; // Calculamos el porcentaje de altura (0% a 100%)
+    const waterHeight = (volume / 10) * 100; 
     water.style.height = `${waterHeight}%`;
 }
 
@@ -30,16 +29,13 @@ function animateWaterFall(direction) {
     const waterFall = document.querySelector('.water-fall');
 
     if (direction === 'increase') {
-        // Solo mostrar el chorro al incrementar el volumen
         waterFall.style.animation = 'waterFallEffect 0.5s ease-out forwards';
 
-        // Resetear la animación del chorro después de que se haya completado
         setTimeout(() => {
             waterFall.style.animation = '';
         }, 500);
     }
 }
 
-// Vincular las funciones a los botones
 document.getElementById('increase-button').addEventListener('click', increaseVolume);
 document.getElementById('decrease-button').addEventListener('click', decreaseVolume);
